cd5fc696bb5ef1e4a29cbc1f449a99e7eb45c371
Jonathan Rocha <jonathan.rocha@msn.com>
2020-10-28 20:51:16 -0300

    Add bonus property to accounts

--- api/data/data.js
+++ api/data/data.js
@@ -1,15 +1,18 @@
 let accounts = [
     {
         id: "1",
-        balance: "10"
+        balance: "10",
+        bonus: "1"
     },
     {
         id: "2",
-        balance: "100"
+        balance: "100",
+        bonus: "2"
     },
     {
         id: "3",
-        balance: "1000"
+        balance: "1000",
+        bonus: "3"
     },
 ];
 
